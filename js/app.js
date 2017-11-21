898f3aae270d1955c6071f2cb57c8e073d958f08
yuanhjty <yuanhjty@gmail.com>
2017-11-21 19:22:49 +0800

    Refactor: Refine codes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Generic Functions
 function addLoadEvent(func) {
     var oldonload = window.onload;
     if (typeof oldonload !== 'function') {
@@ -10,29 +11,24 @@ function addLoadEvent(func) {
     }
 }
 
-var cards = [
-    'diamond', 'anchor', 'bolt', 'bicycle',
-    'bolt', 'bicycle', 'anchor', 'diamond',
-    'paper-plane-o', 'cube', 'leaf', 'cube',
-    'bomb', 'leaf', 'bomb', 'paper-plane-o',
-];
+function addClass(element, className) {
+    var classValue = element.getAttribute('class');
+    if (classValue.indexOf(className) === -1) {
+        classValue = classValue ? classValue + ' ' + className : className;
+        element.setAttribute('class', classValue);
+    }
+}
 
-const CARD_NAME_PREF = 'fa fa-';
-
-var cardsDeck = document.getElementsByClassName('deck')[0];
-var cardElements = cardsDeck.getElementsByTagName('li');
-
-var scorePanel = document.getElementsByClassName('score-panel')[0];
-var stars = scorePanel.getElementsByClassName('stars')[0];
-var starFonts = stars.getElementsByTagName('i');
-var movesElement = scorePanel.getElementsByClassName('moves')[0];
-var timesElements = scorePanel.getElementsByClassName('times')[0];
-
-var movesValue = 0;
-var timesValue = 0;
-var timer = null;
-var currentCardPair = [];
-var matchNumber = 0;
+function removeClass(element, className) {
+    var classValue = element.getAttribute('class');
+    var index = classValue.indexOf(className);
+    if (index !== -1) {
+        var deleteCount = classValue[index + className.length] === ' ' ?
+            className.length + 1 : className.length;
+        classValue = classValue.slice(0, index) + classValue.slice(index + deleteCount);
+        element.setAttribute('class', classValue.trim());
+    }
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -47,146 +43,191 @@ function shuffle(array) {
     }
 }
 
+// Global Objects of Dom Elements to operate
+var cards = function () {
+    var cardsDeck = document.getElementsByClassName('deck')[0];
+    return {
+        NAME_PREF: 'fa fa-',
+        contents: [
+            'diamond', 'anchor', 'bolt', 'bicycle',
+            'bolt', 'bicycle', 'anchor', 'diamond',
+            'paper-plane-o', 'cube', 'leaf', 'cube',
+            'bomb', 'leaf', 'bomb', 'paper-plane-o',
+        ],
+        elements: cardsDeck.getElementsByTagName('li'),
+        firstOfCurrentPair: null,
+        matchPairsNumber: 0
+    };
+}();
 
-function setCard(cardElement, cardName) {
-    var cardFont = cardElement.getElementsByTagName('i')[0];
-    cardFont.setAttribute('class', cardName);
-}
+var scorePanel = function () {
+    var scorePanel = document.getElementsByClassName('score-panel')[0];
+    var starsDeck = scorePanel.getElementsByClassName('stars')[0];
+    return {
+        stars: starsDeck.getElementsByTagName('i'),
+        moves: scorePanel.getElementsByClassName('moves')[0],
+        times: scorePanel.getElementsByClassName('times')[0],
+        movesValue: 0,
+        timesValue: 0,
+        timer: null,
+        restart: scorePanel.getElementsByClassName('restart')[0]
+    };
+}();
 
-function addClass(element, className) {
-    var classValue = element.getAttribute('class');
-    classValue = classValue ? classValue + ' ' + className : className;
-    element.setAttribute('class', classValue);
+var finalScore = function () {
+    var finalScore = document.getElementsByClassName('final-score')[0];
+    var starsDeck = finalScore.getElementsByClassName('stars')[0];
+    return {
+        panel: finalScore,
+        stars: starsDeck.getElementsByTagName('i'),
+        moves: finalScore.getElementsByClassName('final-moves')[0],
+        times: finalScore.getElementsByClassName('final-times')[0],
+        playAgain: finalScore.getElementsByClassName('play-again')[0]
+    };
+}();
+
+// Game Logical Functions
+function setCard(element, cardName) {
+    var card = element.getElementsByTagName('i')[0];
+    card.setAttribute('class', cardName);
 }
 
 function openCard() {
-    if (timer === null) {
-        timer = setInterval(function () {
-            timesValue += 1;
-            timesElements.firstChild.nodeValue = timesValue.toString(10);
+    // 在每一局游戏中，当打开第一张卡片时，启动计时器
+    if (scorePanel.timer === null) {
+        scorePanel.timer = setInterval(function () {
+            scorePanel.timesValue += 1;
+            scorePanel.times.firstChild.nodeValue = scorePanel.timesValue.toString(10);
         }, 1000);
     }
 
     if (this.getAttribute('class') === 'card') {
-        addClass(this, 'open show animated flipInY');
-        currentCardPair.push(this);
+        addClass(this, 'open animated flipInY');
+        if (cards.firstOfCurrentPair === null) {
+            cards.firstOfCurrentPair = this;
+        } else {
+            // Deal with current card pair.
+            (function (firstCard, secondCard) {
+                setTimeout(function () {
+                    dealWithCardPair(firstCard, secondCard);
+                }, 300);
+            })(cards.firstOfCurrentPair, this);
 
-        if (currentCardPair.length === 2) {
-            var firstCard = currentCardPair[0];
-            var secondCard = currentCardPair[1];
-            var firstFont = firstCard.getElementsByTagName('i')[0];
-            var secondFont = secondCard.getElementsByTagName('i')[0];
-            var firstFontClass = firstFont.getAttribute('class');
-            var secondFontClass = secondFont.getAttribute('class');
-            currentCardPair.splice(0, 2);
-            movesValue += 1;
-            movesElement.firstChild.nodeValue = movesValue.toString(10);
-            updateStars(starFonts);
+            // Update moves of the score panel.
+            scorePanel.movesValue += 1;
+            scorePanel.moves.firstChild.nodeValue = scorePanel.movesValue.toString(10);
 
-            setTimeout(function () {
-                if (firstFontClass === secondFontClass) {
-                    firstCard.setAttribute('class', 'card match animated bounce');
-                    secondCard.setAttribute('class', 'card match animated bounce');
-                    matchNumber += 1;
-                    if (matchNumber === 8) {
-                        setTimeout(gameOver, 200);
-                    }
-                } else {
-                    firstCard.setAttribute('class', 'card open show not-match animated shake');
-                    secondCard.setAttribute('class', 'card pen show not-match animated shake');
-                    setTimeout(function () {
-                        firstCard.setAttribute('class', 'card');
-                        secondCard.setAttribute('class', 'card');
-                    }, 400);
-                }
-            }, 300);
+            // Update stars of the score panel.
+            if (scorePanel.movesValue > 13 && scorePanel.movesValue <= 21) {
+                addClass(scorePanel.stars[2], 'lost-stars');
+            } else if (scorePanel.movesValue > 21) {
+                addClass(scorePanel.stars[1], 'lost-stars');
+            }
+
+            // Reset current card pair.
+            cards.firstOfCurrentPair = null;
         }
     }
 }
 
-function updateStars(starFonts) {
-    if (movesValue > 13 && movesValue <= 21) {
-        addClass(starFonts[2], 'lost-stars');
-    } else if (movesValue > 21) {
-        addClass(starFonts[2], 'lost-stars');
-        addClass(starFonts[1], 'lost-stars');
+function dealWithCardPair(firstCard, secondCard) {
+    removeClass(firstCard, 'animated flipInY');
+    removeClass(secondCard, 'animated flipInY');
+
+    var firstCardContent = firstCard.getElementsByTagName('i')[0].getAttribute('class');
+    var secondCardContent = secondCard.getElementsByTagName('i')[0].getAttribute('class');
+
+    if (firstCardContent === secondCardContent) {
+        addClass(firstCard, 'match animated bounce');
+        addClass(secondCard, 'match animated bounce');
+        cards.matchPairsNumber += 1;
+        if (cards.matchPairsNumber === 8) {
+            setTimeout(gameOver, 200);
+        }
+    } else {
+        addClass(firstCard, 'not-match animated shake');
+        addClass(secondCard, 'not-match animated shake');
+        setTimeout(function () {
+            firstCard.setAttribute('class', 'card');
+            secondCard.setAttribute('class', 'card');
+        }, 400);
     }
 }
 
 function gameOver() {
-    clearInterval(timer);
-    timer = null;
-
-    var overMessage = document.getElementsByClassName('over-message')[0];
-    var overStars = overMessage.getElementsByClassName('stars')[0];
-    var overStarFonts = overStars.getElementsByTagName('i');
-    updateStars(overStarFonts);
-
-    var overMoves = overMessage.getElementsByClassName('over-moves')[0];
-    overMoves.firstChild.nodeValue = movesValue.toString(10);
-
-    var overTimes = overMessage.getElementsByClassName('over-times')[0];
-    overTimes.firstChild.nodeValue = timesValue.toString(10);
-
-    overMessage.setAttribute('class', 'over-message show-over-message animated bounceInDown');
-}
-
-function bindClickEvent() {
-    var btnRestart = document.getElementsByClassName('restart')[0];
-    btnRestart.onclick = init;
-
-    for (var i = 0; i < cardElements.length; i++) {
-        cardElements[i].onclick = openCard;
+    // Stop timing.
+    if (scorePanel.timer !== null) {
+        clearInterval(scorePanel.timer);
+        scorePanel.timer = null;
     }
 
-    var tryAgain = document.getElementsByClassName('try-again')[0];
-    tryAgain.onclick = init;
-}
-
-function initOverMessage() {
-    var overMessage = document.getElementsByClassName('over-message')[0];
-    var overStars = overMessage.getElementsByClassName('stars')[0];
-    var overStarFonts = overStars.getElementsByTagName('i');
-    for (var i = 0; i < overStarFonts.length; i++) {
-        overStarFonts[i].setAttribute('class', 'fa fa-star fa-4x');
+    // Update stars of final score.
+    if (scorePanel.movesValue > 13 && scorePanel.movesValue <= 21) {
+        addClass(finalScore.stars[2], 'lost-stars');
+    } else if (scorePanel.movesValue > 21) {
+        addClass(finalScore.stars[2], 'lost-stars');
+        addClass(finalScore.stars[1], 'lost-stars');
     }
-    overMessage.setAttribute('class', 'over-message hide-over-message')
+
+    // Update moves and times of final score.
+    finalScore.moves.firstChild.nodeValue = scorePanel.movesValue.toString(10);
+    finalScore.times.firstChild.nodeValue = scorePanel.timesValue.toString(10);
+
+    // Show final score.
+    addClass(finalScore.panel, 'show animated bounceInDown');
 }
 
-function initCardsDeck() {
-    shuffle(cards);
-    var cardElement, cardName;
-    for (var i = 0; i < cardElements.length; i++) {
-        cardElement = cardElements[i];
-        cardName = CARD_NAME_PREF + cards[i];
-        setCard(cardElements[i], cardName);
-        cardElement.setAttribute('class', 'card');
+// Initialization Functions
+function setEvents() {
+    scorePanel.restart.onclick = init;
+    finalScore.playAgain.onclick = init;
+    for (var i = 0; i < cards.elements.length; i++) {
+        cards.elements[i].onclick = openCard;
     }
 }
 
 function initScorePanel() {
-    for (var i = 0; i < starFonts.length; i++) {
-        starFonts[i].setAttribute('class', 'fa fa-star');
+    for (var i = 0; i < scorePanel.stars.length; i++) {
+        scorePanel.stars[i].setAttribute('class', 'fa fa-star');
     }
 
-    movesValue = 0;
-    timesValue = 0;
-    movesElement.firstChild.nodeValue = movesValue.toString(10);
-    timesElements.firstChild.nodeValue = timesValue.toString(10);
+    scorePanel.movesValue = 0;
+    scorePanel.timesValue = 0;
+    scorePanel.moves.firstChild.nodeValue = '0';
+    scorePanel.times.firstChild.nodeValue = '0';
 
-    if (timer !== null) {
-        clearInterval(timer);
-        timer = null;
+    if (scorePanel.timer !== null) {
+        clearInterval(scorePanel.timer);
+        scorePanel.timer = null;
+    }
+}
+
+function initCards() {
+    shuffle(cards.contents);
+    var cardElement, cardName;
+    for (var i = 0; i < cards.elements.length; i++) {
+        cardElement = cards.elements[i];
+        cardName = cards.NAME_PREF + cards.contents[i];
+        setCard(cardElement, cardName);
+        cardElement.setAttribute('class', 'card');
+    }
+    cards.firstOfCurrentPair = null;
+    cards.matchPairsNumber = 0;
+}
+
+function initFinalScore() {
+    finalScore.panel.setAttribute('class', 'final-score');
+    removeClass(finalScore.panel, 'show');
+    for (var i = 0; i < finalScore.stars.length; i++) {
+        finalScore.stars[i].setAttribute('class', 'fa fa-star fa-4x');
     }
 }
 
 function init() {
     initScorePanel();
-    initCardsDeck();
-    initOverMessage();
-    matchNumber = 0;
-    currentCardPair.splice(0);
+    initCards();
+    initFinalScore();
 }
 
 addLoadEvent(init);
-addLoadEvent(bindClickEvent);
+addLoadEvent(setEvents);
